e128ccb395fafeb3c97121bfec5d81ba5172d0f6
Andrew Aldrich <aldricaj@mail.uc.edu>
2019-09-18 18:53:27 -0400

    Added methods

--- src/flask_routes.py
+++ src/flask_routes.py
@@ -2,19 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login_page():
     return 'hello'
 
-@app.route('/user/<user_id>')
+@app.route('/user/<user_id>', methods=['GET', 'POST'])
 def user_page(user_id):
     return "User: " + str(user_id);
 
-@app.route('/user/<user_id>/document')
+@app.route('/user/<user_id>/document', methods=['GET', 'POST'])
 def view_documents(user_id):
     return "User: " + str(user_id) + " Documents"
 
-@app.route('/user/<user_id>/document/<document_id>')
+@app.route('/user/<user_id>/document/<document_id>', methods=['GET', 'POST'])
 def view_document(user_id, document_id):
     return "User: " + str(user_id) + " Document " + str(document_id)
 
